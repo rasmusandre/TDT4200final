0d6e1d1bf4c0b7c96ccf171dd42b0b1b4e2837eb
Rasmus Tranås <rasmusat@clab23.idi.ntnu.no>
2018-11-12 11:03:45 +0100

    ud

---
+++ build/CMakeFiles/gpurender.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/shomed/r/rasmusat/TDT4200final/TDT4200-Assignment-5/src/common/lodepng.cpp" "/home/shomed/r/rasmusat/TDT4200final/TDT4200-Assignment-5/build/CMakeFiles/gpurender.dir/src/common/lodepng.cpp.o"
+  "/home/shomed/r/rasmusat/TDT4200final/TDT4200-Assignment-5/src/gpu/utilities/OBJLoader.cpp" "/home/shomed/r/rasmusat/TDT4200final/TDT4200-Assignment-5/build/CMakeFiles/gpurender.dir/src/gpu/utilities/OBJLoader.cpp.o"
+  "/home/shomed/r/rasmusat/TDT4200final/TDT4200-Assignment-5/src/gpu/utilities/geometry.cpp" "/home/shomed/r/rasmusat/TDT4200final/TDT4200-Assignment-5/build/CMakeFiles/gpurender.dir/src/gpu/utilities/geometry.cpp.o"
+  "/home/shomed/r/rasmusat/TDT4200final/TDT4200-Assignment-5/src/main.cpp" "/home/shomed/r/rasmusat/TDT4200final/TDT4200-Assignment-5/build/CMakeFiles/gpurender.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "PROJECT_SOURCE_DIR=\"/home/shomed/r/rasmusat/TDT4200final/TDT4200-Assignment-5\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
